961cdb60157ba2bd426936c01c9cb258bf66a2e1
Qing <qingqigao12@gmail.com>
2020-01-15 14:56:12 -0700

    ray test

---
+++ GhostMirror/Assets/Scripts/RaySelect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaySelect : MonoBehaviour
+{
+    public GameObject testLight;
+    private Vector3 hitpos = new Vector3();
+    private Sprite mySprite;
+    private GameObject previousLight;
+    public GameObject lightSource;
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        if(Physics.Raycast(lightSource.transform.position,new Vector3(1,0,1),out hit))
+        {
+            //Transform hitObject = hit.transform;
+           hitpos = new Vector3(hit.point.x+0.05f,hit.point.y+0.05f,hit.point.z);
+            createLight();
+        }
+    }
+    void createLight()
+    {
+        if(previousLight == null)
+        {
+            previousLight = (GameObject)Instantiate(testLight, hitpos, Quaternion.identity);
+            print("!");
+        }
+        else
+        {
+            Destroy(previousLight);
+            previousLight = (GameObject)Instantiate(testLight, hitpos, Quaternion.identity);
+            print("!");
+        }
+        
+    }
+}

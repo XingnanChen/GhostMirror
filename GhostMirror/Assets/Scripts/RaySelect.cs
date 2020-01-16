9cd58d67453c2a78c7dfa539c8dda4ebbc9c2b3e
Qing <qingqigao12@gmail.com>
2020-01-16 12:11:12 -0700

    change test light

--- GhostMirror/Assets/Scripts/RaySelect.cs
+++ GhostMirror/Assets/Scripts/RaySelect.cs
@@ -11,6 +11,7 @@ public class RaySelect : MonoBehaviour
     public GameObject lightSource;
     // Update is called once per frame
     private Rigidbody rb;
+    private Vector3 gyroRotation;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +24,7 @@ public class RaySelect : MonoBehaviour
     
     void Update()
     {
-        Vector3 gyroRotation = Input.gyro.attitude.eulerAngles;
+        gyroRotation = Input.gyro.attitude.eulerAngles;
         gyroRotation = new Vector3(-gyroRotation.x, -gyroRotation.y, gyroRotation.z);
         lightSource.transform.eulerAngles = gyroRotation;
         lightSource.transform.Rotate(Vector3.right * 90, Space.World);
@@ -35,6 +36,8 @@ public class RaySelect : MonoBehaviour
             createLight();
         }
         Debug.DrawRay(lightSource.transform.position, lightSource.transform.forward,Color.red,(1f/60f));
+        testLight.transform.eulerAngles = gyroRotation;
+        testLight.transform.Rotate(Vector3.right * 90, Space.World);
        // Vector3 gyroRot = Input.gyro.rotationRate * rotateSpeed;
 
         
@@ -44,11 +47,15 @@ public class RaySelect : MonoBehaviour
         if(previousLight == null)
         {
             previousLight = (GameObject)Instantiate(testLight, hitpos, Quaternion.identity);
+            previousLight.transform.eulerAngles = gyroRotation;
+            previousLight.transform.Rotate(Vector3.right * 180, Space.World);
         }
         else
         {
             Destroy(previousLight);
             previousLight = (GameObject)Instantiate(testLight, hitpos, Quaternion.identity);
+            previousLight.transform.eulerAngles = gyroRotation;
+            previousLight.transform.Rotate(Vector3.right * 180, Space.World);
         }
         
     }

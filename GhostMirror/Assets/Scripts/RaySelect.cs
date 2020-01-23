b7df05ad792b30eb0c7aa3076d1b7418a083370d
Xingnan <u1274876@utah.edu>
2020-01-22 19:25:42 -0700

    keyboard input

--- GhostMirror/Assets/Scripts/RaySelect.cs
+++ GhostMirror/Assets/Scripts/RaySelect.cs
@@ -13,6 +13,19 @@ public class RaySelect : MonoBehaviour
     // Update is called once per frame
     private Rigidbody rb;
 
+    public bool IsMobile;
+
+    public KeyCode Up;
+    public KeyCode Down;
+    public KeyCode Left;
+    public KeyCode Right;
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
     private string cameraDir;
 
     [SerializeField] private AudioClip doorLocked;
@@ -22,15 +35,39 @@ public class RaySelect : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         Input.gyro.enabled = true;
+
     }
 
     // Update is called once per frame
     
     void Update()
     {
-        if (Input.gyro.enabled)
+        if(!IsMobile)
         {
-            lightSource.transform.rotation = ConvertRotation(Input.gyro.attitude);
+            if (Input.GetKey(Up))
+            {
+                lightSource.transform.Rotate(-40 * Time.deltaTime, 0.0f, 0.0f);
+            }
+            if (Input.GetKey(Down))
+            {
+                lightSource.transform.Rotate(40 * Time.deltaTime, 0.0f, 0.0f);
+            }
+            if (Input.GetKey(Left))
+            {
+                lightSource.transform.Rotate(0.0f, -40 * Time.deltaTime, 0.0f);
+
+            }
+            if (Input.GetKey(Right))
+            {
+                lightSource.transform.Rotate(0.0f, 40 * Time.deltaTime, 0.0f);
+
+            }
+
+            //yaw += speedH * Input.GetAxis("Mouse X");
+            //pitch -= speedV * Input.GetAxis("Mouse Y");
+            //lightSource.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+            // lightSource.transform.rotation = ConvertRotation(Input.gyro.attitude);
             RaycastHit hit;
             if (Physics.Raycast(lightSource.transform.position, lightSource.transform.forward, out hit))
             {
@@ -39,15 +76,29 @@ public class RaySelect : MonoBehaviour
                 createLight();
             }
             Debug.DrawRay(lightSource.transform.position, lightSource.transform.forward, Color.red, (1f / 60f));
-           // testLight.transform.rotation = lightSource.transform.rotation;
-            // Vector3 gyroRot = Input.gyro.rotationRate * rotateSpeed;
-            if (GameObject.Find("door").GetComponent<BoxCollider>().bounds.Contains(hitpos))
+
+        }
+        else
+        {
+            if (Input.gyro.enabled)
             {
-        //        SoundManager.Instance.PlaySFX(doorLocked);
+                lightSource.transform.rotation = ConvertRotation(Input.gyro.attitude);
+                RaycastHit hit;
+                if (Physics.Raycast(lightSource.transform.position, lightSource.transform.forward, out hit))
+                {
+                    //Transform hitObject = hit.transform;
+                    hitpos = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+                    createLight();
+                }
+                Debug.DrawRay(lightSource.transform.position, lightSource.transform.forward, Color.red, (1f / 60f));
+                // testLight.transform.rotation = lightSource.transform.rotation;
+                // Vector3 gyroRot = Input.gyro.rotationRate * rotateSpeed;
+                if (GameObject.Find("door").GetComponent<BoxCollider>().bounds.Contains(hitpos))
+                {
+                    //        SoundManager.Instance.PlaySFX(doorLocked);
+                }
             }
         }
-
-
 
     }
     void createLight()
@@ -83,6 +134,13 @@ public class RaySelect : MonoBehaviour
             Quaternion target = Quaternion.Euler(0, -90, 0);
             camera.transform.rotation = Quaternion.Slerp(camera.transform.rotation, target, Time.deltaTime*smooth);
            // print(camera.transform.rotation);
+            return Quaternion.Euler(90, -90, 0) * (new Quaternion(-q.x, -q.y, q.z, q.w));
+        }
+        else if (cameraDir.Equals("Down"))
+        {
+            Quaternion target = Quaternion.Euler(0, -90, 0);
+            camera.transform.rotation = Quaternion.Slerp(camera.transform.rotation, target, Time.deltaTime * smooth);
+            // print(camera.transform.rotation);
             return Quaternion.Euler(90, -90, 0) * (new Quaternion(-q.x, -q.y, q.z, q.w));
         }
         else
